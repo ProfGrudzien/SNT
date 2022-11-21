a1328f57a03445915fe208a2d30cf29b9dc6e1b8
Grudzien <70582964+ProfGrudzien@users.noreply.github.com>
2022-11-21 16:03:31 +0100

    Add files via upload

---
+++ 2_PhotoNumerique/Bayer.js
@@ -0,0 +1,28 @@
+const tableBayer = document.getElementById("Bayer")
+const btnBayer = document.getElementById("btnBayer")
+const pBayer = document.getElementById("pBayer")
+const couleurs = ["blanc", "vert", "rouge", "bleu"]
+
+function changeCouleur(event) {
+    if (btnBayer.style.display != "none") {
+        const index = couleurs.indexOf(event.target.className) + 1
+        if (index > 0) {
+            event.target.className = couleurs[index % couleurs.length]
+        }
+    }
+}
+
+function verifier(event) {
+    const texte = Array.from(tableBayer.getElementsByTagName("TD"))
+     .map(element => element.className == "blanc" ? "w" : element.className[0])
+     .join("")
+    if (texte == "vrvrvrvrbvbvbvbvvrvrvrvrbvbvbvbv") {
+        btnBayer.style.display = "None"
+        pBayer.textContent = "Bravo, vous avec reconstité le filtre de Bayer."
+    } else {
+        pBayer.textContent = "Réessayer !"
+    }
+}
+
+tableBayer.addEventListener("click", changeCouleur)
+btnBayer.addEventListener("click", verifier)
